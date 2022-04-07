72176d2ae524ba8d0957c0abed54cb2dfba01b27
dleszczynski19 <dleszczynski@sii.pl>
2022-04-07 13:00:55 +0200

    MVP Shadow roots

---
+++ src/test/java/com/configuration/TestBase.java
@@ -0,0 +1,37 @@
+package com.configuration;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
+
+public class TestBase {
+    public WebDriver driver;
+    private static Logger log = LoggerFactory.getLogger(TestBase.class);
+    public static Marker passed = MarkerFactory.getMarker("PASSED");
+
+    @BeforeAll
+    static void setup() {
+        WebDriverManager.chromedriver().setup();
+        log.info("Driver setup properly");
+    }
+
+    @BeforeEach
+    void initializeDriver() {
+        driver = new ChromeDriver(new ChromeOptions().addArguments("start-maximized"));
+        log.info("Driver initialized properly");
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        log.info("Driver closed properly");
+    }
+}
---
+++ src/test/java/com/test/ShadowRootTest.java
@@ -0,0 +1,28 @@
+package com.test;
+
+import com.configuration.TestBase;
+import com.google.downloads.DownloadPage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ShadowRootTest extends TestBase {
+    private static Logger log = LoggerFactory.getLogger(ShadowRootTest.class);
+
+    @Test
+    @DisplayName("Shadow root test")
+    public void shouldManageShadowRoot() {
+
+        DownloadPage downloadPage = new DownloadPage(driver);
+
+        driver.get("chrome://downloads/");
+        log.info("First Shadow Root Element: " + downloadPage.getShadowRootElement("downloads-toolbar").getText());
+        log.info("Second Shadow Root Element: " + downloadPage.getShadowRootElement("cr-toolbar").getText());
+        log.info("Third Shadow Root Element: " + downloadPage.getShadowRootElement("cr-icon-button").getAttribute("id"));
+        log.info("Fourth Shadow Root Element: " + downloadPage.getShadowRootElement("#moreActions #icon iron-icon").getTagName());
+        downloadPage.getShadowRootElement("#moreActions #icon iron-icon").click();
+        log.info("Fifth Shadow Root Element: " + downloadPage
+                .getShadowRootElement("#moreActions #icon iron-icon svg").getAttribute("style"));
+    }
+}
